14344c762ac31a41d61c23de55073758f5af4e5e
Brian Diesel <bri@ndiesel.net>
2016-07-28 12:43:18 -0400

    adding download for full size SVHN images and DigiStuct class

--- projects/capstone/svhn_data.py
+++ projects/capstone/svhn_data.py
@@ -3,6 +3,10 @@ import struct
 import numpy as np
 import scipy
 import sys
+import tarfile
+
+
+from digit_struct import DigitStruct
 
 from scipy.io import loadmat
 from sklearn.cross_validation import train_test_split
@@ -14,20 +18,33 @@ from six.moves.urllib.request import urlretrieve
 #import matplotlib.pyplot as plt
 #from random import randint
 
-data_path = "data/svhn"
-cropped_data_path = data_path+"/cropped"
-full = data_path+"/full"
+DATA_PATH = "data/svhn"
+CROPPED_DATA_PATH = DATA_PATH+"/cropped"
+FULL_DATA_PATH = DATA_PATH+"/full"
 FORMAT_2_FILES = ['{}_32x32.mat'.format(s) for s in ['train', 'test', 'extra']]
-FORMAT_2_TRAIN_FILE, FORMAT_2_TEST_FILE, FORMAT_2_EXTRA_FILE = FORMAT_2_FILES
 PIXEL_DEPTH = 255
 NUM_LABELS = 10
 last_percent_reported = None
 
 def read_data_file(file_name):
-	file = open(file_name, 'rb')
-	data = process_data_file(file)
-	file.close()
+	if file_name.endswith("tar.gz"):
+		extract_data_file(file_name)
+		raise NotImplementedError('Need to do something after data is loaded!')
+	else:
+		file = open(file_name, 'rb')
+		data = process_data_file(file)
+		file.close()
 	return data
+
+def extract_data_file(file_name):
+	tar = tarfile.open(file_name, "r:gz")
+	tar.extractall(FULL_DATA_PATH)
+	tar.close()
+
+def read_digit_struct(dataset):
+	path = FULL_DATA_PATH +"/"+dataset
+	struct_file = os.path.join(path, "digitStruct.mat")
+	return DigitStruct(struct_file)
 
 def convert_img_array_vec(img_array):
 	rows = img_array.shape[0]
@@ -71,24 +88,46 @@ def process_data_file(file):
 	img_array = convert_img_array_vec(imgs)
 	return img_array, labels_one_hot
 
-def create_svhn(dataset):
-	if dataset == "train":
-		data_file_name = "train_32x32.mat"
-	elif dataset == "test":
-		data_file_name =  "test_32x32.mat"
-	elif dataset == "extra":
-		data_file_name = "extra_32x32.mat"
+def get_data_file_name(master_set, dataset):
+	if master_set == "cropped":
+		if dataset == "train":
+			data_file_name = "train_32x32.mat"
+		elif dataset == "test":
+			data_file_name = "test_32x32.mat"
+		elif dataset == "extra":
+			data_file_name = "extra_32x32.mat"
+		else:
+			raise NotImplementedError('dataset must be either train, test or extra')
+	elif master_set == "full":
+		if dataset == "train":
+			data_file_name = "train.tar.gz"
+		elif dataset == "test":
+			data_file_name = "test.tar.gz"
+		elif dataset == "extra":
+			data_file_name = "extra.tar.gz"
 	else:
-		raise NotImplementedError('dataset must be either train or test')
-	data_file = os.path.join(cropped_data_path, data_file_name)
-	if os.path.exists(data_file):
-		X, y = read_data_file(data_file)
+		raise NotImplementedError('Master data set must be full or cropped')
+	return data_file_name;
+
+def make_data_dirs(master_set):
+	if master_set == "cropped":
+		os.makedirs(CROPPED_DATA_PATH)
+	elif master_set == "full":
+		os.makedirs(FULL_DATA_PATH)
 	else:
-		if not os.path.exists(cropped_data_path):
-			os.makedirs(cropped_data_path)
-		downloaded_file = download_data_file(data_file_name)
-		X, y = read_data_file(downloaded_file)
-	return X, y
+		raise NotImplementedError('Master data set must be full or cropped')
+
+def create_svhn(master_set, dataset):
+	path = FULL_DATA_PATH
+	data_file_name = get_data_file_name(master_set, dataset)
+	data_file_pointer = os.path.join(path, data_file_name)
+	if os.path.exists(data_file_pointer):
+		return read_data_file(data_file_pointer)
+	else:
+		if not os.path.exists(path):
+			make_data_dirs(master_set)
+		downloaded_file = download_data_file(path, data_file_name)
+		return read_data_file(downloaded_file)
 
 def download_progress(count, block_size, total_size):
 	global last_percent_reported
@@ -102,10 +141,10 @@ def download_progress(count, block_size, total_size):
 			sys.stdout.flush()
 		last_percent_reported = percent
 
-def download_data_file(filename, force=False):
+def download_data_file(path, filename, force=False):
 	base_url = "http://ufldl.stanford.edu/housenumbers/"
 	print "Attempting to download", filename
-	saved_file, _ = urlretrieve(base_url + filename, os.path.join(cropped_data_path, filename), reporthook=download_progress)
+	saved_file, _ = urlretrieve(base_url + filename, os.path.join(path, filename), reporthook=download_progress)
 	print("\nDownload Complete!")
 	statinfo = os.stat(saved_file)
 	if statinfo.st_size == get_expected_bytes(filename):
@@ -121,8 +160,14 @@ def get_expected_bytes(filename):
 		byte_size = 64275384
 	elif filename == "extra_32x32.mat":
 		byte_size = 1329278602
+	elif filename == "test.tar.gz":
+		byte_size = 276555967
+	elif filename == "train.tar.gz":
+		byte_size = 404141560
+	elif filename == "extra.tar.gz":
+		byte_size = 1955489752
 	else:
-		raise Exception("Invalid file name" + filename)
+		raise Exception("Invalid file name " + filename)
 	return byte_size
 
 def train_validation_spit(train_dataset, train_labels):
@@ -130,29 +175,36 @@ def train_validation_spit(train_dataset, train_labels):
 	return train_dataset, validation_dataset, train_labels, validation_labels
 
 def write_npy_file(data_array, lbl_array, data_set_name):
-	np.save(os.path.join(cropped_data_path,data_set_name+'_svhn_imgs.npy'), data_array)
+	np.save(os.path.join(CROPPED_DATA_PATH,data_set_name+'_svhn_imgs.npy'), data_array)
 	print 'Saving to %s_svhn_imgs.npy file done. Data size is: %s' %((data_set_name), data_array.shape)
-	np.save(os.path.join(cropped_data_path,data_set_name+'_svhn_labels.npy'), lbl_array)
+	np.save(os.path.join(CROPPED_DATA_PATH,data_set_name+'_svhn_labels.npy'), lbl_array)
 	print 'Saving to %s_svhn_labels.npy file done. Contains %d rows' %(data_set_name, len(lbl_array))
 
 def load_svhn_data(data_type):
 	if data_type == "training":
-		imgs = np.load(os.path.join(cropped_data_path, 'train_svhn_imgs.npy'))
-		labels = np.load(os.path.join(cropped_data_path, 'train_svhn_labels.npy'))
+		imgs = np.load(os.path.join(CROPPED_DATA_PATH, 'train_svhn_imgs.npy'))
+		labels = np.load(os.path.join(CROPPED_DATA_PATH, 'train_svhn_labels.npy'))
 	elif data_type == "testing":
-		imgs = np.load(os.path.join(cropped_data_path, 'test_svhn_imgs.npy'))
-		labels = np.load(os.path.join(cropped_data_path, 'test_svhn_labels.npy'))
+		imgs = np.load(os.path.join(CROPPED_DATA_PATH, 'test_svhn_imgs.npy'))
+		labels = np.load(os.path.join(CROPPED_DATA_PATH, 'test_svhn_labels.npy'))
 	elif(data_type == "validation"):
-		imgs = np.load(os.path.join(cropped_data_path, 'valid_svhn_imgs.npy'))
-		labels = np.load(os.path.join(cropped_data_path, 'valid_svhn_labels.npy'))
+		imgs = np.load(os.path.join(CROPPED_DATA_PATH, 'valid_svhn_imgs.npy'))
+		labels = np.load(os.path.join(CROPPED_DATA_PATH, 'valid_svhn_labels.npy'))
 	else:
 		raise Exception("Data Set not found!")
 	return imgs, labels
 
 if __name__ == '__main__':
-	train_data, train_labels = create_svhn('train')
-	test_data, test_labels = create_svhn('test')
-	#train_data, valid_data, train_labels, valid_labels = train_validation_spit(train_data, train_labels)	
-	write_npy_file(train_data, train_labels, "train")
-	#write_npy_file(valid_data, valid_labels, "valid")
-	write_npy_file(test_data, test_labels, "test")
+	# train_data, train_labels = create_svhn('cropped', 'train')
+	# test_data, test_labels = create_svhn('cropped', 'test')
+	# #train_data, valid_data, train_labels, valid_labels = train_validation_spit(train_data, train_labels)	
+	# write_npy_file(train_data, train_labels, "train")
+	# #write_npy_file(valid_data, valid_labels, "valid")
+	# write_npy_file(test_data, test_labels, "test")
+	#test_data, test_labels = create_svhn('full', 'test')
+	#train_data, train_labels = create_svhn('full', 'train')
+	#extra_data, extra_labels = create_svhn('full', 'extra')
+	#train_full = read_digit_struct("train")
+	#test_full = read_digit_struct("test")
+	#print(train_full, test_full)
+
--- projects/capstone/svhn_with_summaries.py
+++ projects/capstone/svhn_with_summaries.py
@@ -1,25 +1,3 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""A simple MNIST classifier which displays summaries in TensorBoard.
-
- This is an unimpressive MNIST model, but it is a good example of using
-tf.name_scope to make a graph legible in the TensorBoard graph explorer, and of
-naming summary tags so that they are grouped meaningfully in TensorBoard.
-
-It demonstrates the functionality of every TensorBoard dashboard.
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -28,71 +6,70 @@ import tensorflow as tf
 from pdb import set_trace as bp
 from svhn_data import load_svhn_data
 
-learning_rate = .001
-drop_out = 0.9
-max_steps = 2000
-data_dir = '/tmp/data'
-summaries_dir = '/tmp/svhn_logs'
+#Constants
+LEARNING_RATE = .001
+DROP_OUT = 0.9
+MAX_STEPS = 2000
+TENSORBOARD_SUMMARIES_DIR = '/tmp/svhn_logs'
 IMG_ROWS = 32
 IMG_COLS = 32
 NUM_CHANNELS = 3
 NUM_LABELS = 10
-BATCH_SIZE = 10000
+BATCH_SIZE = 1024
 
-
+'''Load training files from svhn_data.py utility.'''
 train_X, train_y = load_svhn_data("training")
 valid_X, valid_y = load_svhn_data("validation")
 test_X, test_y = load_svhn_data("testing")
 
-def train(x, y_):
-  # Create a multilayer model.
+# Create a multilayer model.
+# We can't initialize these variables to 0 - the network will get stuck.
+def weight_variable(shape):
+  """Create a weight variable with appropriate initialization."""
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
 
-  # We can't initialize these variables to 0 - the network will get stuck.
-  def weight_variable(shape):
-    """Create a weight variable with appropriate initialization."""
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+def bias_variable(shape):
+  """Create a bias variable with appropriate initialization."""
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
 
-  def bias_variable(shape):
-    """Create a bias variable with appropriate initialization."""
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+def variable_summaries(var, name):
+  """Attach a lot of summaries to a Tensor."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.scalar_summary('mean/' + name, mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+    tf.scalar_summary('sttdev/' + name, stddev)
+    tf.scalar_summary('max/' + name, tf.reduce_max(var))
+    tf.scalar_summary('min/' + name, tf.reduce_min(var))
+    tf.histogram_summary(name, var)
 
-  def variable_summaries(var, name):
-    """Attach a lot of summaries to a Tensor."""
-    with tf.name_scope('summaries'):
-      mean = tf.reduce_mean(var)
-      tf.scalar_summary('mean/' + name, mean)
-      with tf.name_scope('stddev'):
-        stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-      tf.scalar_summary('sttdev/' + name, stddev)
-      tf.scalar_summary('max/' + name, tf.reduce_max(var))
-      tf.scalar_summary('min/' + name, tf.reduce_min(var))
-      tf.histogram_summary(name, var)
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+  """Reusable code for making a simple neural net layer.
+  It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+  It also sets up name scoping so that the resultant graph is easy to read,
+  and adds a number of summary ops.
+  """
+  # Adding a name scope ensures logical grouping of the layers in the graph.
+  with tf.name_scope(layer_name):
+    # This Variable will hold the state of the weights for the layer
+    with tf.name_scope('weights'):
+      weights = weight_variable([input_dim, output_dim])
+      variable_summaries(weights, layer_name + '/weights')
+    with tf.name_scope('biases'):
+      biases = bias_variable([output_dim])
+      variable_summaries(biases, layer_name + '/biases')
+    with tf.name_scope('Wx_plus_b'):
+      preactivate = tf.matmul(input_tensor, weights) + biases
+      tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+    activations = act(preactivate, 'activation')
+    tf.histogram_summary(layer_name + '/activations', activations)
+    return activations
 
-  def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
-    """Reusable code for making a simple neural net layer.
-
-    It does a matrix multiply, bias add, and then uses relu to nonlinearize.
-    It also sets up name scoping so that the resultant graph is easy to read,
-    and adds a number of summary ops.
-    """
-    # Adding a name scope ensures logical grouping of the layers in the graph.
-    with tf.name_scope(layer_name):
-      # This Variable will hold the state of the weights for the layer
-      with tf.name_scope('weights'):
-        weights = weight_variable([input_dim, output_dim])
-        variable_summaries(weights, layer_name + '/weights')
-      with tf.name_scope('biases'):
-        biases = bias_variable([output_dim])
-        variable_summaries(biases, layer_name + '/biases')
-      with tf.name_scope('Wx_plus_b'):
-        preactivate = tf.matmul(input_tensor, weights) + biases
-        tf.histogram_summary(layer_name + '/pre_activations', preactivate)
-      activations = act(preactivate, 'activation')
-      tf.histogram_summary(layer_name + '/activations', activations)
-      return activations
-
+def model(x, y_):
+  #create a 2-layer model with dropout
   hidden1 = nn_layer(x, 3072, 500, 'layer1')
 
   with tf.name_scope('dropout'):
@@ -109,7 +86,7 @@ def train(x, y_):
     tf.scalar_summary('cross entropy', cross_entropy)
 
   with tf.name_scope('train'):
-    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
 
   with tf.name_scope('accuracy'):
     with tf.name_scope('correct_prediction'):
@@ -120,18 +97,16 @@ def train(x, y_):
   return accuracy, keep_prob, train_step
 
 def predict(accuracy, x, y_, keep_prob, train_step):
-  #mnist = input_data.read_data_sets(data_dir, one_hot=True)
   # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
   with tf.Session() as sess:
     merged = tf.merge_all_summaries()
-    train_writer = tf.train.SummaryWriter(summaries_dir + '/train',sess.graph)
-    test_writer = tf.train.SummaryWriter(summaries_dir + '/test')
+    train_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/train',sess.graph)
+    test_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/test')
     tf.initialize_all_variables().run()
     # Train the model, and also write summaries.
     # Every 10th step, measure test-set accuracy, and write test summaries
     # All other steps, run train_step on training data, & add training summaries
-    
-    for i in range(max_steps):
+    for i in range(MAX_STEPS):
       if i % 10 == 0:  # Record summaries and test-set accuracy
         summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(x, y_, keep_prob, i, False))
         test_writer.add_summary(summary, i)
@@ -150,33 +125,29 @@ def predict(accuracy, x, y_, keep_prob, train_step):
 
 def batched_data(idx, imgs, labels, ds):
   """Return the next `batch_size` examples from this data set."""
-  #start = idx * BATCH_SIZE
   start = idx + BATCH_SIZE
   end = start + BATCH_SIZE
   batched_images = imgs[start:end]
   batched_labels = labels[start:end]
-  #print(batched_images.shape, batched_labels.shape, ds, start, end)
   return batched_images, batched_labels
 
-
 def feed_dict(x, y_, keep_prob, idx, train):
-  """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
-  # offset = (idx * BATCH_SIZE) % (y_.shape[0] - BATCH_SIZE)
-  # batch_data = x[offset:(offset + BATCH_SIZE), :, :, :]
-  # batch_labels = y_[offset:(offset + BATCH_SIZE), :]
+  #TODO fix batching
+  """Creates a TensorFlow feed_dict: maps data into Tensor placeholders."""
   if train:
     xs, ys = batched_data(idx, train_X, train_y, "TRAIN")
-    k = drop_out
+    k = DROP_OUT
   else:
     xs, ys = batched_data(idx, test_X, test_y, "TEST")
     k = 1.0
   return {x: xs, y_: ys, keep_prob: k}
 
-
 def prepare_log_dir():
-  if tf.gfile.Exists(summaries_dir):
-    tf.gfile.DeleteRecursively(summaries_dir)
-  tf.gfile.MakeDirs(summaries_dir)
+  '''Clears the log files then creates new directories to place the 
+     tensorbard log file.'''
+  if tf.gfile.Exists(TENSORBOARD_SUMMARIES_DIR):
+    tf.gfile.DeleteRecursively(TENSORBOARD_SUMMARIES_DIR)
+  tf.gfile.MakeDirs(TENSORBOARD_SUMMARIES_DIR)
 
 def main(_):
   prepare_log_dir()
@@ -185,14 +156,14 @@ def main(_):
   # Input placeholders
   with tf.name_scope('input'):
     row_len = IMG_ROWS * IMG_COLS * NUM_CHANNELS
-    x = tf.placeholder(tf.float32, [BATCH_SIZE, row_len], name='x-input')
-    y_ = tf.placeholder(tf.float32, [BATCH_SIZE, NUM_LABELS], name='y-input')
-  
+    x = tf.placeholder(tf.float32, [BATCH_SIZE, row_len], name='Images')
+    y_ = tf.placeholder(tf.float32, [BATCH_SIZE, NUM_LABELS], name='Labels')
+
   with tf.name_scope('input_reshape'):
     image_shaped_input = tf.reshape(x, [-1, IMG_ROWS, IMG_COLS, NUM_CHANNELS])
     tf.image_summary('input', image_shaped_input, 100)
-  
-  accuracy, keep_prob, train_step = train(x, y_)
+
+  accuracy, keep_prob, train_step = model(x, y_)
   predict(accuracy, x, y_, keep_prob, train_step)
 
 if __name__ == '__main__':
