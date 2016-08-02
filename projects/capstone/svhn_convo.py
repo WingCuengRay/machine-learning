5bb596a4fa4dfe037222d54e6c0e8727cd4313f4
Brian Diesel <bri@ndiesel.net>
2016-08-01 21:23:12 -0400

    Adding: Feature for Tensorboard graphing for one dataset at a time

--- projects/capstone/svhn_convo.py
+++ projects/capstone/svhn_convo.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import time
 import numpy
 import sys
@@ -12,28 +11,19 @@ import tensorflow as tf
 from svhn_data import load_svhn_data
 from svhn_model import model
 from pdb import set_trace as bp
-#from mnist_data import load_mnist_data
 
 TENSORBOARD_SUMMARIES_DIR = '/tmp/svhn_convo_logs'
 NUM_LABELS = 10
 VALIDATION_SIZE = 5000  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
 BATCH_SIZE = 64
-NUM_EPOCHS = 2
+NUM_EPOCHS = 1
 EVAL_BATCH_SIZE = 64
 EVAL_FREQUENCY = 100  # Number of steps between evaluations.
-IMG_ROWS = 32
-IMG_COLS = 32
-NUM_CHANNELS = 3
 
 def error_rate(predictions, labels):
-  """Return the error rate based on dense predictions and sparse labels."""
-  with tf.name_scope('accuracy'):
-    with tf.name_scope('correct_prediction'):
-      correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(labels, 1))
-  with tf.name_scope('accuracy'):
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  tf.scalar_summary('accuracy', accuracy)
+  correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(labels, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   return accuracy.eval() * 100
 
 def prepare_log_dir():
@@ -66,39 +56,18 @@ def eval_in_batches(data, sess, eval_prediction, eval_data):
       predictions[begin:, :] = batch_predictions[begin - size:, :]
   return predictions
 
-def train(initial_weights=None):
-  pass
-
-def main(saved_weights_path):
-  print("SWP", saved_weights_path)
-  prepare_log_dir()
-  
-  train_data, train_labels= load_svhn_data("training")
-  valid_data, valid_labels = load_svhn_data("validation")
-  test_data, test_labels = load_svhn_data("testing")
-  train_size = train_labels.shape[0]
-
+def train(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path):
   # This is where training samples and labels are fed to the graph.
-  # These placeholder nodes will be fed a batch of training data at each
-  # training step using the {feed_dict} argument to the Run() call below.
-  with tf.name_scope('input'):
-    tf_data_node = tf.placeholder(tf.float32, shape=[BATCH_SIZE, IMG_ROWS, IMG_COLS, NUM_CHANNELS])
-    tf_labels_node = tf.placeholder(tf.float32, shape=[BATCH_SIZE, NUM_LABELS])
-    #eval_data = tf.placeholder(tf.float32,  shape=(EVAL_BATCH_SIZE, IMG_ROWS, IMG_COLS, NUM_CHANNELS))
-    #eval_data = tf_data_node
-
-  with tf.name_scope('image'):
-    tf.image_summary('input', tf_data_node, 100)
+  y_ = tf.placeholder(tf.float32, shape=[BATCH_SIZE, NUM_LABELS])
 
   # Training computation: logits + cross-entropy loss.
-  logits, params = model(tf_data_node, True)
+  x, logits, params = model(True)
   train_prediction = tf.nn.softmax(logits)
 
   # Predictions for the test and validation, which we'll compute less often.
-  sm_pred, params = model(tf_data_node)
-  eval_prediction = tf.nn.softmax(sm_pred)
+  #x, sm_pred, params = model()
+  #eval_prediction = tf.nn.softmax(sm_pred)
 
-  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_labels_node))
   # L2 regularization for the fully connected parameters.
   l3_weights = params[4]
   l3_biases = params[5]
@@ -106,6 +75,8 @@ def main(saved_weights_path):
   l4_biases = params[7]
   regularizers = (tf.nn.l2_loss(l3_weights) + tf.nn.l2_loss(l3_biases) + tf.nn.l2_loss(l4_weights) + tf.nn.l2_loss(l4_biases))
   
+  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_))
+
   # Add the regularization term to the loss.
   loss += 5e-4 * regularizers
 
@@ -120,23 +91,20 @@ def main(saved_weights_path):
       train_size,          # Decay step.
       0.95,                # Decay rate.
       staircase=True)
-  # Use simple momentum for the optimization.
   tf.scalar_summary('learning_rate', learning_rate)
-  optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(loss, global_step=batch)
-  # Predictions for the current training minibatch.
 
-  
+  # Use simple momentum for the optimization.
+  optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(loss, global_step=batch)
 
   #tf.initialize_all_variables().run()
   init_op = tf.initialize_all_variables()
 
   # Add ops to save and restore all the variables.
   saver = tf.train.Saver()
-  
+
   # Create a local session to run the training.
   start_time = time.time()
   with tf.Session() as sess:
-    
     # Restore variables from disk.
     if(saved_weights_path):
       saver.restore(sess, saved_weights_path)
@@ -144,58 +112,78 @@ def main(saved_weights_path):
 
     sess.run(init_op)
     #Run all the initializers to prepare the trainable parameters.
+    # Add histograms for trainable variables.
+    for var in tf.trainable_variables():
+      tf.histogram_summary(var.op.name, var)
+
+    #Add accuracy to tesnosrboard
+    with tf.name_scope('accuracy'):
+      with tf.name_scope('correct_prediction'):
+        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
+      with tf.name_scope('accuracy'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+      tf.scalar_summary('accuracy', accuracy)
+
     #Prepare vairables for the tensorboard
     merged = tf.merge_all_summaries()
-    train_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/train',sess.graph)
+    
+    train_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/train', sess.graph)
     test_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/test')
+    
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
 
-    print('Initialized!')
     # Loop through training steps.
-
     for step in xrange(int(NUM_EPOCHS * train_size) // BATCH_SIZE):
       # Compute the offset of the current minibatch in the data.
       # Note that we could use better randomization across epochs.
       offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
+
       batch_data = train_data[offset:(offset + BATCH_SIZE), ...]
       batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
-      # This dictionary maps the batch data (as a numpy array) to the
-      # node in the graph it should be fed to.
-      feed_dict = {tf_data_node: batch_data, tf_labels_node: batch_labels}
+
+      # This dictionary maps the batch data (as a numpy array) to the      
+      feed_dict = {x: batch_data, y_: batch_labels}
+
       # Run the graph and fetch some of the nodes.
-      
       summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict)
       
       if step % EVAL_FREQUENCY == 0:
         elapsed_time = time.time() - start_time
         start_time = time.time()
-        print('Step %d (epoch %.2f), %.1f ms' %
-              (step, float(step) * BATCH_SIZE / train_size, 1000 * elapsed_time / EVAL_FREQUENCY))
-        print("Of Step:", int(NUM_EPOCHS * train_size))
+        print('Step %d of Step %d (epoch %.2f), %.1f ms' %
+              (step, int(NUM_EPOCHS * train_size), float(step) * BATCH_SIZE / train_size, 1000 * elapsed_time / EVAL_FREQUENCY))
        
         print('Minibatch loss: %.2f, learning rate: %.6f' % (l, lr))
         print('Minibatch Accuracy: %.1f%%' % error_rate(predictions, batch_labels))
-        print('Validation Accuracy: %.2f%%' % error_rate(eval_in_batches(valid_data, sess, eval_prediction, tf_data_node), valid_labels))
-        sys.stdout.flush()
-    
-      if step % 10 == 0:  # Record summaries and test-set accuracy
-        test_writer.add_summary(summary, step)
-
-      if step % EVAL_FREQUENCY == 99:
-        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-        run_metadata = tf.RunMetadata()
-        summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict, options=run_options, run_metadata=run_metadata)
         train_writer.add_run_metadata(run_metadata, 'step%03d' % step)
         train_writer.add_summary(summary, step)
-        print('Adding run metadata for', step)
-        # Save the variables to disk.
-        save_path = saver.save(sess, "/tmp/model.ckpt")
-        print("Model saved in file: %s" % save_path)
+        #print('Validation Accuracy: %.2f%%' % error_rate(eval_in_batches(valid_data, sess, eval_prediction, tf_data_node), valid_labels))
+        sys.stdout.flush()
+    
+      # if step % 10 == 0:  # Record summaries and test-set accuracy
+      #   test_writer.add_summary(summary, step)
+      # if step % EVAL_FREQUENCY == 99:
+      #   summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict, options=run_options, run_metadata=run_metadata)
+      #   print('Adding run metadata for', step)
 
     # Finally print the result!
     train_writer.close()
     test_writer.close()
-    test_error = error_rate(eval_in_batches(test_data, sess, eval_prediction, tf_data_node), test_labels)
+    #Save the variables to disk.
+    save_path = saver.save(sess, "/tmp/model.ckpt")
+    print("Model saved in file: %s" % save_path)
+    #test_error = error_rate(eval_in_batches(test_data, sess, eval_prediction, tf_data_node), test_labels)
     #print('Test Accuracy: %.2f%%' % test_error)
+
+def main(saved_weights_path):
+  prepare_log_dir()
+  train_data, train_labels= load_svhn_data("training")
+  valid_data, valid_labels = load_svhn_data("validation")
+  test_data, test_labels = load_svhn_data("testing")
+  train_size = train_labels.shape[0]
+  train(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path)
+  
 
 if __name__ == '__main__':
   saved_weights_path = None
--- projects/capstone/svhn_model.py
+++ projects/capstone/svhn_model.py
@@ -14,101 +14,28 @@ IMAGE_SIZE = 32
 NUM_CHANNELS = 3
 NUM_LABELS = 10
 BATCH_SIZE = 64
+IMG_ROWS = 32
+IMG_COLS = 32
+NUM_CHANNELS = 3
+
 # EVAL_BATCH_SIZE = 128
 # SEED = None
-
-#Utilites
-def weight_variable(shape):
-	"""Create a weight variable with appropriate initialization."""
-	initial = tf.truncated_normal(shape, stddev=0.1)
-	return tf.Variable(initial)
-
-def bias_variable(shape):
-	"""Create a bias variable with appropriate initialization."""
-	initial = tf.constant(0.1, shape=shape)
-	return tf.Variable(initial)
-
-def conv2d(x, W, stride=(1, 1), padding='SAME'):
-	return tf.nn.conv2d(x, W, strides=[1, 2, 2, 1], padding=padding)
-
-def max_pool(x, ksize=(2, 2), stride=(2, 2)):
-	return tf.nn.max_pool(x, ksize=[1, ksize[0], ksize[1], 1], strides=[1, stride[0], stride[1], 1], padding='SAME')
-
-def dropout(layer, keep_prob):
-	with tf.name_scope('dropout'):
-		tf.scalar_summary('dropout_keep_probability', keep_prob)
-		dropped = tf.nn.dropout(layer, keep_prob)
-	return dropped
-
-def cross_entropy(y, y_):
-	with tf.name_scope('cross_entropy'):
-		diff = y_ * tf.log(y)
-		with tf.name_scope('total'):
-			cross_entropy = -tf.reduce_mean(diff)
-		tf.scalar_summary('cross entropy', cross_entropy)
-		return cross_entropy
-
-
-def variable_summaries(var, name):
-  """Attach a lot of summaries to a Tensor."""
-  with tf.name_scope('summaries'):
-    mean = tf.reduce_mean(var)
-    tf.scalar_summary('mean/' + name, mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-  tf.scalar_summary('sttdev/' + name, stddev)
-  tf.scalar_summary('max/' + name, tf.reduce_max(var))
-  tf.scalar_summary('min/' + name, tf.reduce_min(var))
-  tf.histogram_summary(name, var)
-
-
-# Create a multilayer model.
-def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
-  """Reusable code for making a simple neural net layer.
-  It does a matrix multiply, bias add, and then uses relu to nonlinearize.
-  It also sets up name scoping so that the resultant graph is easy to read,
-  and adds a number of summary ops.
-  """
-  # Adding a name scope ensures logical grouping of the layers in the graph.
-  with tf.name_scope(layer_name):
-    # This Variable will hold the state of the weights for the layer
-    with tf.name_scope('weights'):
-      weights = weight_variable(input_dim)
-      variable_summaries(weights, layer_name + '/weights')
-    with tf.name_scope('biases'):
-      biases = bias_variable([output_dim])
-      variable_summaries(biases, layer_name + '/biases')
-    with tf.name_scope('convolution'):     
-      preactivate = conv2d(input_tensor, weights) + biases
-      tf.histogram_summary(layer_name + '/pre_activations', preactivate)
-    activations = act(preactivate, 'activation')
-    tf.histogram_summary(layer_name + '/activations', activations)
-    return activations
-
-
 def _activation_summary(x):
-  """Helper to create summaries for activations.
-
-  Creates a summary that provides a histogram of activations.
-  Creates a summary that measure the sparsity of activations.
-
-  Args:
-    x: Tensor
-  Returns:
-    nothing
-  """
-  # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
-  # session. This helps the clarity of presentation on tensorboard.
-  #tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
   tensor_name = x.op.name
   tf.histogram_summary(tensor_name + '/activations', x)
   tf.scalar_summary(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
 
-
-
 # We will replicate the model structure for the training subgraph, as well
 # as the evaluation subgraphs, while sharing the trainable parameters.
-def model(data, train=False):
+def model(train=False):
+  # These placeholder nodes will be fed a batch of training data at each
+  # training step using the {feed_dict} argument to the Run() call below.
+  with tf.name_scope('input'):
+    data_node = tf.placeholder(tf.float32, shape=[BATCH_SIZE, IMG_ROWS, IMG_COLS, NUM_CHANNELS])
+    
+  with tf.name_scope('image'):
+    tf.image_summary('input', data_node, 100)
+
   # The variables below hold all the trainable weights. They are passed an
   # initial value which will be assigned when we call:
   # {tf.initialize_all_variables().run()}
@@ -130,10 +57,11 @@ def model(data, train=False):
   # shape matches the data layout: [image index, y, x, depth].
   
   #Layer 1
-  if NUM_CHANNELS == 1:
-    data = tf.expand_dims(data, 3)
+  if NUM_CHANNELS == 1: #If a greysacle image is sent expand the dimensions
+    data_node = tf.expand_dims(data_node, 3)
+  
   with tf.variable_scope('conv1') as scope:
-    conv1 = tf.nn.conv2d(data, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+    conv1 = tf.nn.conv2d(data_node, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
     # Bias and rectified linear non-linearity.
     relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
     _activation_summary(relu1)
@@ -173,9 +101,85 @@ def model(data, train=False):
     softmax_linear = tf.add(tf.matmul(l4, sm_weights), sm_biases)
     #_activation_summary(softmax_linear)
 
-  return softmax_linear, [conv1_weights, conv1_biases, conv2_weights, conv2_biases, l3_weights, l3_biases, l4_weights, l4_biases]
+  return data_node, softmax_linear, [conv1_weights, conv1_biases, conv2_weights, conv2_biases, l3_weights, l3_biases, l4_weights, l4_biases]
 
 
+
+
+
+
+
+
+
+
+
+#Utilites
+def weight_variable(shape):
+  """Create a weight variable with appropriate initialization."""
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  """Create a bias variable with appropriate initialization."""
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W, stride=(1, 1), padding='SAME'):
+  return tf.nn.conv2d(x, W, strides=[1, 2, 2, 1], padding=padding)
+
+def max_pool(x, ksize=(2, 2), stride=(2, 2)):
+  return tf.nn.max_pool(x, ksize=[1, ksize[0], ksize[1], 1], strides=[1, stride[0], stride[1], 1], padding='SAME')
+
+def dropout(layer, keep_prob):
+  with tf.name_scope('dropout'):
+    tf.scalar_summary('dropout_keep_probability', keep_prob)
+    dropped = tf.nn.dropout(layer, keep_prob)
+  return dropped
+
+def cross_entropy(y, y_):
+  with tf.name_scope('cross_entropy'):
+    diff = y_ * tf.log(y)
+    with tf.name_scope('total'):
+      cross_entropy = -tf.reduce_mean(diff)
+    tf.scalar_summary('cross entropy', cross_entropy)
+    return cross_entropy
+
+
+def variable_summaries(var, name):
+  """Attach a lot of summaries to a Tensor."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.scalar_summary('mean/' + name, mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+  tf.scalar_summary('sttdev/' + name, stddev)
+  tf.scalar_summary('max/' + name, tf.reduce_max(var))
+  tf.scalar_summary('min/' + name, tf.reduce_min(var))
+  tf.histogram_summary(name, var)
+
+
+# Create a multilayer model.
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+  """Reusable code for making a simple neural net layer.
+  It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+  It also sets up name scoping so that the resultant graph is easy to read,
+  and adds a number of summary ops.
+  """
+  # Adding a name scope ensures logical grouping of the layers in the graph.
+  with tf.name_scope(layer_name):
+    # This Variable will hold the state of the weights for the layer
+    with tf.name_scope('weights'):
+      weights = weight_variable(input_dim)
+      variable_summaries(weights, layer_name + '/weights')
+    with tf.name_scope('biases'):
+      biases = bias_variable([output_dim])
+      variable_summaries(biases, layer_name + '/biases')
+    with tf.name_scope('convolution'):     
+      preactivate = conv2d(input_tensor, weights) + biases
+      tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+    activations = act(preactivate, 'activation')
+    tf.histogram_summary(layer_name + '/activations', activations)
+    return activations
 
 
 
