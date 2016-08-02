1fc3ed1e24d7a95016215e3e123ee8330741cb5f
Brian Diesel <bri@ndiesel.net>
2016-08-02 15:26:20 -0400

    Adding: Feature for restoring weights into a model for digit detection.

--- projects/capstone/svhn_detect.py
+++ projects/capstone/svhn_detect.py
@@ -5,7 +5,9 @@ import numpy as np
 import tensorflow as tf
 import pylab
 import matplotlib
-from matplotlib import pyplot as plt
+
+#from matplotlib import pyplot as plt
+from svhn_model import model
 
 from pdb import set_trace as bp
 
@@ -14,7 +16,18 @@ def detect(img_path, saved_model_weights):
   #print("hola", img_path)
   img = cv2.imread(img_path, 3)
   img_data = matplotlib.pyplot.imshow(img)
-  pylab.show(img_data)
+  #pylab.show()
+ 
+  #load the previously saved model to load the vars into
+  x, y, params = model()
+
+  #create a loader
+  saver = tf.train.Saver()
+  with tf.Session() as sess:
+    print("Loading Saved Checkpoints From:", saved_model_weights)
+    #place the weights into the model.
+    saver.restore(sess, saved_model_weights)
+    print("Model restored.")
 
 
 if __name__ == "__main__":
