fdeb5d70f966dbba5ea3b0e53a86bce97099e4d6
Brian Diesel <bri@ndiesel.net>
2016-08-02 12:01:06 -0400

    Adding: Feature for loading validation and training data.

--- projects/capstone/svhn_convo.py
+++ projects/capstone/svhn_convo.py
@@ -17,7 +17,7 @@ NUM_LABELS = 10
 VALIDATION_SIZE = 5000  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
 BATCH_SIZE = 64
-NUM_EPOCHS = 1
+NUM_EPOCHS = 3
 EVAL_BATCH_SIZE = 64
 EVAL_FREQUENCY = 100  # Number of steps between evaluations.
 
@@ -33,28 +33,16 @@ def prepare_log_dir():
     tf.gfile.DeleteRecursively(TENSORBOARD_SUMMARIES_DIR)
   tf.gfile.MakeDirs(TENSORBOARD_SUMMARIES_DIR)
 
-# Small utility function to evaluate a dataset by feeding batches of data to
-# {eval_data} and pulling the results from {eval_predictions}.
-# Saves memory and enables this to run on smaller GPUs.
-def eval_in_batches(data, sess, eval_prediction, eval_data):
-  """Get all predictions for a dataset by running it in small batches."""
-  size = data.shape[0]
-  if size < BATCH_SIZE:
-    raise ValueError("batch size for evals larger than dataset: %d" % size)
-  predictions = numpy.ndarray(shape=(size, NUM_LABELS), dtype=numpy.float32)
-  
-  for begin in xrange(0, size, BATCH_SIZE):
-    end = begin + BATCH_SIZE
-    if end <= size:
-      predictions[begin:end, :] = sess.run(
-          eval_prediction,
-          feed_dict={eval_data: data[begin:end, ...]})
-    else:
-      batch_predictions = sess.run(
-          eval_prediction,
-          feed_dict={eval_data: data[-BATCH_SIZE:, ...]})
-      predictions[begin:, :] = batch_predictions[begin - size:, :]
-  return predictions
+def fill_feed_dict(train_data, train_labels, x, y_, step):
+  train_size = train_labels.shape[0]
+  # Compute the offset of the current minibatch in the data.
+  # Note that we could use better randomization across epochs.
+  offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
+
+  batch_data = train_data[offset:(offset + BATCH_SIZE), ...]
+  batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
+
+  return {x: batch_data, y_: batch_labels}
 
 def train(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path):
   # This is where training samples and labels are fed to the graph.
@@ -62,11 +50,7 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
 
   # Training computation: logits + cross-entropy loss.
   x, logits, params = model(True)
-  train_prediction = tf.nn.softmax(logits)
-
-  # Predictions for the test and validation, which we'll compute less often.
-  #x, sm_pred, params = model()
-  #eval_prediction = tf.nn.softmax(sm_pred)
+  prediction = tf.nn.softmax(logits)
 
   # L2 regularization for the fully connected parameters.
   l3_weights = params[4]
@@ -74,7 +58,7 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
   l4_weights = params[6]
   l4_biases = params[7]
   regularizers = (tf.nn.l2_loss(l3_weights) + tf.nn.l2_loss(l3_biases) + tf.nn.l2_loss(l4_weights) + tf.nn.l2_loss(l4_biases))
-  
+
   loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_))
 
   # Add the regularization term to the loss.
@@ -111,7 +95,7 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
       print("Model restored.")
 
     sess.run(init_op)
-    #Run all the initializers to prepare the trainable parameters.
+    # Run all the initializers to prepare the trainable parameters.
     # Add histograms for trainable variables.
     for var in tf.trainable_variables():
       tf.histogram_summary(var.op.name, var)
@@ -128,6 +112,7 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
     merged = tf.merge_all_summaries()
     
     train_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/train', sess.graph)
+    valid_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/validation')
     test_writer = tf.train.SummaryWriter(TENSORBOARD_SUMMARIES_DIR + '/test')
     
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
@@ -135,44 +120,44 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
 
     # Loop through training steps.
     for step in xrange(int(NUM_EPOCHS * train_size) // BATCH_SIZE):
-      # Compute the offset of the current minibatch in the data.
-      # Note that we could use better randomization across epochs.
-      offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
-
-      batch_data = train_data[offset:(offset + BATCH_SIZE), ...]
-      batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
-
-      # This dictionary maps the batch data (as a numpy array) to the      
-      feed_dict = {x: batch_data, y_: batch_labels}
-
       # Run the graph and fetch some of the nodes.
-      summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict)
+      # This dictionary maps the batch data (as a numpy array) to the
+      feed_dict = fill_feed_dict(train_data, train_labels, x, y_, step)
+
+      summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], 
+        feed_dict = feed_dict)
       
-      if step % EVAL_FREQUENCY == 0:
+      if step % EVAL_FREQUENCY == 10:
         elapsed_time = time.time() - start_time
         start_time = time.time()
         print('Step %d of Step %d (epoch %.2f), %.1f ms' %
               (step, int(NUM_EPOCHS * train_size), float(step) * BATCH_SIZE / train_size, 1000 * elapsed_time / EVAL_FREQUENCY))
-       
         print('Minibatch loss: %.2f, learning rate: %.6f' % (l, lr))
-        print('Minibatch Accuracy: %.1f%%' % error_rate(predictions, batch_labels))
+        print('Minibatch Accuracy: %.1f%%' % error_rate(predictions, feed_dict[y_]))
         train_writer.add_run_metadata(run_metadata, 'step%03d' % step)
         train_writer.add_summary(summary, step)
-        #print('Validation Accuracy: %.2f%%' % error_rate(eval_in_batches(valid_data, sess, eval_prediction, tf_data_node), valid_labels))
         sys.stdout.flush()
     
-      # if step % 10 == 0:  # Record summaries and test-set accuracy
-      #   test_writer.add_summary(summary, step)
-      # if step % EVAL_FREQUENCY == 99:
-      #   summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict, options=run_options, run_metadata=run_metadata)
-      #   print('Adding run metadata for', step)
+      if step % EVAL_FREQUENCY == 99:
+        valid_feed_dict = fill_feed_dict(valid_data, valid_labels, x, y_, step)
+        summary, _, l, lr, valid_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=valid_feed_dict, options=run_options, run_metadata=run_metadata)
+        valid_writer.add_run_metadata(run_metadata, 'step%03d' % step)
+        valid_writer.add_summary(summary, step)
+        print('Adding run metadata for', step)
+        print('Validation Accuracy: %.2f%%' % error_rate(valid_predictions, feed_dict[y_]))
 
-    # Finally print the result!
-    train_writer.close()
-    test_writer.close()
     #Save the variables to disk.
     save_path = saver.save(sess, "/tmp/model.ckpt")
     print("Model saved in file: %s" % save_path)
+    
+    test_feed_dict = fill_feed_dict(test_data, test_labels, x, y_, step)
+    summary, _, l, lr, test_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=test_feed_dict, options=run_options, run_metadata=run_metadata)
+    test_writer.add_run_metadata(run_metadata, 'step%03d' % step)
+    test_writer.add_summary(summary, step)
+
+    train_writer.close()
+    valid_writer.close()
+    test_writer.close()
     #test_error = error_rate(eval_in_batches(test_data, sess, eval_prediction, tf_data_node), test_labels)
     #print('Test Accuracy: %.2f%%' % test_error)
 
