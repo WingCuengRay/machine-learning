b444f39fc367b5c6211371cde60f8f227ca1f52c
Brian Diesel <bri@ndiesel.net>
2016-08-05 17:00:33 -0400

    Optimize: For running on GPU, off loading process from CPU.

--- projects/capstone/svhn_train.py
+++ projects/capstone/svhn_train.py
@@ -9,16 +9,16 @@ import os
 import tensorflow as tf
 
 from svhn_data import load_svhn_data
+from datetime import datetime
 from svhn_model import classification_head
 from pdb import set_trace as bp
 
 TENSORBOARD_SUMMARIES_DIR = '/tmp/svhn_convo_logs'
 NUM_LABELS = 10
-VALIDATION_SIZE = 5000  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
-BATCH_SIZE = 64
-NUM_EPOCHS = 3
-EVAL_FREQUENCY = 100  # Number of steps between evaluations.
+BATCH_SIZE = 1024
+#NUM_EPOCHS = 256
+NUM_EPOCHS = 50
 
 def error_rate(predictions, labels):
   correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(labels, 1))
@@ -81,13 +81,12 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
 
   #tf.initialize_all_variables().run()
   init_op = tf.initialize_all_variables()
-
   # Add ops to save and restore all the variables.
   saver = tf.train.Saver()
 
   # Create a local session to run the training.
   start_time = time.time()
-  with tf.Session() as sess:
+  with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
     # Restore variables from disk.
     if(saved_weights_path):
       saver.restore(sess, saved_weights_path)
@@ -97,15 +96,15 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
     # Run all the initializers to prepare the trainable parameters.
     # Add histograms for trainable variables.
     for var in tf.trainable_variables():
-      tf.histogram_summary(var.op.name, var)
+     tf.histogram_summary(var.op.name, var)
 
     #Add accuracy to tesnosrboard
     with tf.name_scope('accuracy'):
-      with tf.name_scope('correct_prediction'):
-        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
-      with tf.name_scope('accuracy'):
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-      tf.scalar_summary('accuracy', accuracy)
+     with tf.name_scope('correct_prediction'):
+       correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
+     with tf.name_scope('accuracy'):
+       accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+     tf.scalar_summary('accuracy', accuracy)
 
     #Prepare vairables for the tensorboard
     merged = tf.merge_all_summaries()
@@ -122,36 +121,39 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
       # Run the graph and fetch some of the nodes.
       # This dictionary maps the batch data (as a numpy array) to the
       feed_dict = fill_feed_dict(train_data, train_labels, x, y_, step)
+      _, l, lr, predictions = sess.run([optimizer, loss, learning_rate, prediction], feed_dict = feed_dict)
+      duration = time.time() - start_time
 
-      summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], 
-        feed_dict = feed_dict)
-      
-      if step % EVAL_FREQUENCY == 10:
-        elapsed_time = time.time() - start_time
-        start_time = time.time()
-        print('Step %d of Step %d (epoch %.2f), %.1f ms' %
-              (step, int(NUM_EPOCHS * train_size), float(step) * BATCH_SIZE / train_size, 1000 * elapsed_time / EVAL_FREQUENCY))
-        print('Minibatch loss: %.2f, learning rate: %.6f' % (l, lr))
-        print('Minibatch Accuracy: %.1f%%' % error_rate(predictions, feed_dict[y_]))
-        train_writer.add_run_metadata(run_metadata, 'step%03d' % step)
-        train_writer.add_summary(summary, step)
-        sys.stdout.flush()
-    
-      if step % EVAL_FREQUENCY == 99:
+      if step %  1000 == 0:
         valid_feed_dict = fill_feed_dict(valid_data, valid_labels, x, y_, step)
-        summary, _, l, lr, valid_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=valid_feed_dict, options=run_options, run_metadata=run_metadata)
+        
+        valid_summary, _, l, lr, valid_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=valid_feed_dict, options=run_options, run_metadata=run_metadata)
         valid_writer.add_run_metadata(run_metadata, 'step%03d' % step)
-        valid_writer.add_summary(summary, step)
+        valid_writer.add_summary(valid_summary, step)
+        
+        train_summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict = feed_dict)
+        train_writer.add_run_metadata(run_metadata, 'step%03d' % step)
+        train_writer.add_summary(train_summary, step)
+
         print('Adding run metadata for', step)
         print('Validation Accuracy: %.2f%%' % error_rate(valid_predictions, feed_dict[y_]))
+
+      if step % 10 == 0:
+        elapsed_time = time.time() - start_time
+        start_time = time.time()
+        examples_per_sec = BATCH_SIZE / duration
+        format_str = ('%s: step %d, loss = %.2f  learning rate = %.2f  (%.1f examples/sec; %.3f ''sec/batch)')
+        print (format_str % (datetime.now(), step, l, lr, examples_per_sec, duration))
+        sys.stdout.flush()
 
     #Save the variables to disk.
     save_path = saver.save(sess, "/tmp/model.ckpt")
     print("Model saved in file: %s" % save_path)
     test_feed_dict = fill_feed_dict(test_data, test_labels, x, y_, step)
-    summary, _, l, lr, test_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=test_feed_dict, options=run_options, run_metadata=run_metadata)
+    _, l, lr, test_predictions = sess.run([optimizer, loss, learning_rate, prediction], feed_dict=test_feed_dict, options=run_options, run_metadata=run_metadata)
+    test_summary, _, l, lr, test_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=test_feed_dict, options=run_options, run_metadata=run_metadata)
     test_writer.add_run_metadata(run_metadata, 'step%03d' % step)
-    test_writer.add_summary(summary, step)
+    test_writer.add_summary(test_summary, step)
 
     train_writer.close()
     valid_writer.close()
@@ -164,6 +166,12 @@ def main(saved_weights_path):
   train_data, train_labels= load_svhn_data("training")
   valid_data, valid_labels = load_svhn_data("validation")
   test_data, test_labels = load_svhn_data("testing")
+  
+  #load full sized data
+  #train_data_full, train_labels_full= load_svhn_data("training")
+  #valid_data_full, valid_labels_full = load_svhn_data("validation")
+  #test_data_full, test_labels_full = load_svhn_data("testing")
+
   train_size = train_labels.shape[0]
   train(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path)
   
