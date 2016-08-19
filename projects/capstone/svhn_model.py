661b0334a5b111db974052848e8a07744fa4f114
Brian Diesel <bri@ndiesel.net>
2016-08-18 21:46:12 -0400

    Adding: Classifier with approx 88% accuracy.

--- projects/capstone/svhn_model.py
+++ projects/capstone/svhn_model.py
@@ -1,137 +1,222 @@
 import tensorflow as tf
 from pdb import set_trace as bp
+import numpy as np
 
-IMG_ROWS = 32
-IMG_COLS = 32
 NUM_CHANNELS = 3
+NUM_LABELS = 11 # 0-9, + blank 
+PATCH_SIZE = 5
+DEPTH_1 = 16
+DEPTH_2 = 20
+DEPTH_3 =  20
+DEPTH_4 = 512
+num_hidden1 = 64
+DROPOUT = 0.75
 
 def _activation_summary(x):
   tensor_name = x.op.name
   tf.histogram_summary(tensor_name + '/activations', x)
   tf.scalar_summary(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
 
-# We will replicate the model structure for the training subgraph, as well
-# as the evaluation subgraphs, while sharing the trainable parameters.
-def convolution_model(batch_size, train):
-  # These placeholder nodes will be fed a batch of training data at each
-  # training step using the {feed_dict} argument to the Run() call below.
-  with tf.name_scope('input'):
-    data_node = tf.placeholder(tf.float32, shape=[batch_size, IMG_ROWS, IMG_COLS, NUM_CHANNELS])
-    
-  with tf.name_scope('image'):
-   tf.image_summary('input', data_node, 10)
 
+def convolution_model(data_node, batch_size, train):
+  # We will replicate the model structure for the training subgraph, as well
+  # as the evaluation subgraphs, while sharing the trainable parameters.
   # The variables below hold all the trainable weights. They are passed an
   # initial value which will be assigned when we call:
   # {tf.initialize_all_variables().run()}
-  conv1_weights = tf.Variable(tf.truncated_normal([5, 5, NUM_CHANNELS, 64], stddev=5e-2))
-  conv1_biases =  tf.Variable(tf.zeros([64]))
+  conv1_weights = tf.Variable(tf.truncated_normal([PATCH_SIZE, PATCH_SIZE, NUM_CHANNELS, DEPTH_1], stddev=5e-2))
+  conv1_biases =  tf.Variable(tf.zeros([DEPTH_1]))
 
-  conv2_weights = tf.Variable(tf.truncated_normal(shape=[5, 5, 64, 64], stddev=5e-2))
-  conv2_biases =  tf.Variable(tf.constant(0.1, shape=[64]))
+  conv2_weights = tf.Variable(tf.truncated_normal(shape=[PATCH_SIZE, PATCH_SIZE, DEPTH_1, DEPTH_2], stddev=5e-2))
+  conv2_biases =  tf.Variable(tf.constant(0.1, shape=[DEPTH_2]))
   
-  #conv3_weights = tf.Variable
-  #conv3_biases = tf.Variable
+  conv3_weights = tf.Variable(tf.truncated_normal(shape=[PATCH_SIZE, PATCH_SIZE, DEPTH_2, DEPTH_3], stddev=5e-2))
+  conv3_biases =  tf.Variable(tf.constant(0.1, shape=[DEPTH_3]))
+
+  conv4_weights = tf.Variable(tf.truncated_normal(shape=[PATCH_SIZE, PATCH_SIZE, DEPTH_3, DEPTH_4], stddev=5e-2))
+  conv4_biases =  tf.Variable(tf.constant(0.1, shape=[DEPTH_4]))
+
 
   """The Model definition."""
   # 2D convolution, with 'SAME' padding (i.e. the output feature map has
   # the same size as the input). Note that {strides} is a 4D array whose
   # shape matches the data layout: [image index, y, x, depth].
+  #if NUM_CHANNELS == 1: #If a greysacle image is sent expand the dimensions
+  #  data_node = tf.expand_dims(data_node, 3)
   
-  #Layer 1
-  if NUM_CHANNELS == 1: #If a greysacle image is sent expand the dimensions
-    data_node = tf.expand_dims(data_node, 3)
-  
-  with tf.variable_scope('conv1') as scope:
+  '''Layer 1'''
+  with tf.variable_scope('conv_1') as scope:
     conv1 = tf.nn.conv2d(data_node, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+    print("Conv 1 shape", conv1.get_shape().as_list())
     # Bias and rectified linear non-linearity.
     relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+    print("Relu 1 shape", conv1.get_shape().as_list())
     _activation_summary(relu1)
     # Max pooling. The kernel size spec {ksize} also follows the layout of
     # the data. Here we have a pooling window of 2, and a stride of 2.
-    pool1 = tf.nn.max_pool(relu1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
+    pool1 = tf.nn.max_pool(relu1, ksize=[1, 3, 3, 1], strides=[1,2,2,1], padding='SAME', name='pool1')
     norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm1')
+    print("Pool 1 shape", pool1.get_shape().as_list())
 
-  #Layer 2
-  with tf.variable_scope('conv2') as scope:
-    conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
+  '''Layer 2'''
+  with tf.variable_scope('conv_2') as scope:
+    conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1,1,1,1], padding='SAME')
+    print("Conv 2 shape", conv2.get_shape().as_list())
     relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+    print("Relu 2 shape", relu2.get_shape().as_list())
     _activation_summary(relu2)
     norm2 = tf.nn.lrn(relu2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm2')
-    pool2 = tf.nn.max_pool(norm2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+    pool2 = tf.nn.max_pool(norm2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool2')
+    print("Pool 2 shape", pool2.get_shape().as_list())
 
-  #Layer3
-  # with tf.variable_scope('conv3') as scope:
-  #   conv3 = tf.nn.conv2d(pool2, conv3_weights, strides = [1,1,1,1,], padding='SAME')
-  #   relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
-  #   _activation_summary(relu3)
-  #   norm3 = tf.nn.lrn(relu3, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm3')
-  #   pool3 = tf.nn.max_pool(norm3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool3')
+  '''Layer3'''
+  with tf.variable_scope('conv_3') as scope:
+    conv3 = tf.nn.conv2d(pool2, conv3_weights, strides = [1,1,1,1], padding='SAME')
+    print("Conv 3 shape", conv3.get_shape().as_list())
+    relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
+    print("Relu 3 shape", relu3.get_shape().as_list())
+    _activation_summary(relu3)
+    norm3 = tf.nn.lrn(relu3, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm3')
+    pool3 = tf.nn.max_pool(norm3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool3')
+    print("Pool 3 shape", pool3.get_shape().as_list())
 
-  weights = [conv1_weights, conv1_biases, 
-            conv2_weights, conv2_biases]
+  #'''Layer4'''
+  #with tf.variable_scope('conv_4') as scope:
+  #   conv4 = tf.nn.conv2d(pool3, conv4_weights, strides = [1,1,1,1], padding='SAME')
+  #   relu4 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
+  #   _activation_summary(relu4)
+  #   norm4 = tf.nn.lrn(relu4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm4')
+  #   pool4 = tf.nn.max_pool(norm4, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool4')
 
-  return data_node, pool2, weights
+  #'''Layer5'''
+  # with tf.variable_scope('conv_4') as scope:
+  #   conv5 = tf.nn.conv2d(pool3, conv4_weights, strides = [1,1,1,1], padding='SAME')
+  #   relu5 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
+  #   _activation_summary(relu4)
+  #   norm4 = tf.nn.lrn(relu4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm3')
+  #   pool4 = tf.nn.max_pool(norm4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool3')
+
+  weights = [conv1_weights, conv1_biases, conv2_weights, conv2_biases, conv3_weights, conv3_biases]
+  return pool3, weights
 
 
-def classification_head(batch_size, train=False):
-  x, conv_layer, conv_vars = convolution_model(batch_size, train=False)
 
-  l3_weights = tf.Variable(tf.truncated_normal(shape=[4096, 384], stddev=0.04))
-  l3_biases = tf.Variable(tf.constant(0.1, shape=[384]))
-
-  l4_weights = tf.Variable(tf.truncated_normal(shape=[384, 192], stddev=0.04))
-  l4_biases = tf.Variable(tf.constant(0.1, shape=[192]))
-
-  #Layer3
-  with tf.variable_scope('local3') as scope:
-    reshape = tf.reshape(conv_layer, [batch_size, -1])
-    dim = reshape.get_shape()[1].value
-    l3_weights = tf.Variable(tf.truncated_normal(shape=[dim, 384], stddev=0.04))
-    l3_biases = tf.Variable(tf.constant(0.1, shape=[384]))
-    l3 = tf.nn.relu(tf.matmul(reshape, l3_weights) + l3_biases)
-    _activation_summary(l3)
-
-  #Layer4
-  with tf.variable_scope('local4') as scope:
-    l4_weights = tf.Variable(tf.truncated_normal(shape=[384, 192], stddev=0.04))
-    l4_biases = tf.Variable(tf.constant(0.1, shape=[192]))
-    l4 = tf.nn.relu(tf.matmul(l3, l4_weights) + l4_biases)
-    _activation_summary(l4)
+def classification_head(X, batch_size, train=False):
+  conv_layer, conv_vars = convolution_model(X, batch_size, train=False)
   
-  with tf.variable_scope('softmax_linear') as scope:
-    sm_weights = tf.Variable(tf.truncated_normal(shape=[192, 10], stddev=1/192.0))
-    sm_biases = tf.Variable(tf.constant(0.1, shape=[10]))
-    softmax_linear = tf.add(tf.matmul(l4, sm_weights), sm_biases)
-    _activation_summary(softmax_linear)
+  #Layer3
+  dim = 4*4*20
+  l3_weights = tf.Variable(tf.truncated_normal(shape=[dim, 10], stddev=0.04))
+  l3_biases = tf.Variable(tf.constant(0.1, shape=[10]))
 
-  weights = conv_vars + [l3_weights, l3_biases, 
-            l4_weights, l4_biases]
+  out_weights = tf.Variable(tf.random_normal([10, 10]))
+  out_biases =  tf.Variable(tf.constant(0.1, shape=[10]))
+
+  #Fully Connected Layer
+  with tf.variable_scope('fully_connected') as scope:
+    fc1 = tf.reshape(conv_layer, [batch_size, -1])
+    fc1 = tf.add(tf.matmul(fc1, l3_weights), l3_biases)
+    fc_out = tf.nn.relu(fc1)
+    print("fc shape", fc_out.get_shape().as_list())
+    _activation_summary(fc_out)
+ 
+  out = tf.add(tf.matmul(fc_out, out_weights), out_biases)
+
+  #apply dopout to training.
+  if train == True:
+    print("using drop out")
+    out = tf.nn.dropout(out, DROPOUT)
+  else:
+    print("not using dropout")
+
+  weights = conv_vars + [l3_weights, l3_biases, out_weights, out_biases]
 
   #output class scores 
-  return x, softmax_linear, weights
+  return out, weights
 
-def regression_head(batch_size, train=False):
-  '''takes any size input and slides a window of size [32x32] 
-  across the image in 4x4 strides.'''
-  num_runs = 5
-  num_labels = 10
-  #Densley Connected Layer
-  fc1_weights = tf.Variable(tf.truncated_normal(shape=[4096, 384], stddev=0.04))
-  fc1_biases = tf.Variable(tf.constant(0.1, shape=[384]))
 
-  W_conv1 = tf.reshape(fc1_weights, [8,  32, 128, 2048])
-  h_conv1 = tf.nn.relu(conv2d(conv_layer, reshape,
-                                stride=(1, 1), padding="VALID") + fc1_biases) 
-  #Output Layers
-  #5 max outputs, 10 classes
-  fc2_weights = weight_variable([2048, 1 + num_runs * num_labels])
-  fc2_biases = bias_variable([1 + num_runs * num_labels])
 
-  W_conv2 = tf.reshape(W_fc2, [1, 1, 2048, 1 + num_runs * num_labels])
-  h_conv2 = conv2d(h_conv1, W_conv2) + fc2_biases
 
-  x, conv_layer, conv_vars = convolutional_layers(batch_size, train=False)
+layer1_weights = tf.get_variable("W1", shape=[PATCH_SIZE, PATCH_SIZE, NUM_CHANNELS, DEPTH_1],\
+         initializer=tf.contrib.layers.xavier_initializer_conv2d())
+layer1_biases = tf.Variable(tf.constant(1.0, shape=[DEPTH_1]), name='B1')
 
-  #Output Box Coordinates
-  pass 
+layer2_weights = tf.get_variable("W2", shape=[PATCH_SIZE, PATCH_SIZE, DEPTH_1, DEPTH_2],\
+         initializer=tf.contrib.layers.xavier_initializer_conv2d())
+layer2_biases = tf.Variable(tf.constant(1.0, shape=[DEPTH_2]), name='B2')
+
+layer3_weights = tf.get_variable("W3", shape=[PATCH_SIZE, PATCH_SIZE, DEPTH_2, num_hidden1],\
+         initializer=tf.contrib.layers.xavier_initializer_conv2d())
+layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden1]), name='B3')
+
+layer4_weights = tf.get_variable("W4", shape=[PATCH_SIZE, PATCH_SIZE, 64, 64],\
+          initializer=tf.contrib.layers.xavier_initializer_conv2d())
+layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden1]), name='B4')
+
+
+s1_w = tf.get_variable("WS1", shape=[num_hidden1, NUM_LABELS],\
+         initializer=tf.contrib.layers.xavier_initializer())
+s1_b = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]), name='BS1')
+
+s2_w = tf.get_variable("WS2", shape=[num_hidden1, NUM_LABELS],\
+         initializer=tf.contrib.layers.xavier_initializer())
+s2_b = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]), name='BS2')
+
+s3_w = tf.get_variable("WS3", shape=[num_hidden1, NUM_LABELS],\
+         initializer=tf.contrib.layers.xavier_initializer())
+s3_b = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]), name='BS3')
+
+s4_w = tf.get_variable("WS4", shape=[num_hidden1, NUM_LABELS],\
+         initializer=tf.contrib.layers.xavier_initializer())
+s4_b = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]), name='BS4')
+
+s5_w = tf.get_variable("WS5", shape=[num_hidden1, NUM_LABELS],\
+         initializer=tf.contrib.layers.xavier_initializer())
+s5_b = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]), name='BS5')
+
+
+def regression_head(X, batch_size, train=False):
+  conv = tf.nn.conv2d(X, layer1_weights, [1,1,1,1], 'SAME', name='C1')
+  print("Conv 1 shape", conv.get_shape().as_list())
+  hidden = tf.nn.relu(conv + layer1_biases)
+  lrn = tf.nn.local_response_normalization(hidden)
+  sub = tf.nn.max_pool(lrn, [1,2,2,1], [1,2,2,1], 'SAME', name='S2')
+  print("Layer 1 shape", sub.get_shape().as_list())
+
+  conv = tf.nn.conv2d(sub, layer2_weights, [1,1,1,1], padding='SAME', name='C3')
+  hidden = tf.nn.relu(conv + layer2_biases)
+  lrn = tf.nn.local_response_normalization(hidden)
+  sub = tf.nn.max_pool(lrn, [1,2,2,1], [1,2,2,1], 'SAME', name='S4')
+  print("Layer 2 shape", sub.get_shape().as_list())  
+
+  conv = tf.nn.conv2d(sub, layer3_weights, [1,1,1,1], padding='SAME', name='C5')
+  hidden = tf.nn.relu(conv + layer3_biases)
+  lrn = tf.nn.local_response_normalization(hidden)
+  sub = tf.nn.max_pool(lrn, [1,2,2,1], [1,2,2,1], 'SAME', name='S5')
+  print("Layer 3 shape", sub.get_shape().as_list())
+  
+  #bp()
+  # conv = tf.nn.conv2d(hidden, layer4_weights, [1,1,1,1], padding='SAME', name='C6')
+  # hidden = tf.nn.relu(conv + layer4_biases)
+  # lrn = tf.nn.local_response_normalization(hidden)
+  # sub = tf.nn.max_pool(lrn, [1,2,2,1], [1,2,2,1], 'SAME', name='S6')
+  # print("Layer 4 shape", sub.get_shape().as_list())
+
+
+  #bp()
+
+
+  shape = hidden.get_shape().as_list()
+  reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+    
+  #hidden = tf.nn.relu(tf.matmul(reshape, layer4_weights) + layer4_biases)
+  logits1 = tf.matmul(reshape, s1_w) + s1_b
+  logits2 = tf.matmul(reshape, s2_w) + s2_b
+  logits3 = tf.matmul(reshape, s3_w) + s3_b
+  logits4 = tf.matmul(reshape, s4_w) + s4_b
+  logits5 = tf.matmul(reshape, s5_w) + s5_b
+ 
+  return [logits1, logits2, logits3, logits4, logits5]
+
+
+
--- projects/capstone/train_classifier.py
+++ projects/capstone/train_classifier.py
@@ -13,12 +13,14 @@ from datetime import datetime
 from svhn_model import classification_head
 from pdb import set_trace as bp
 
-TENSORBOARD_SUMMARIES_DIR = '/tmp/svhn_convo_logs'
+TENSORBOARD_SUMMARIES_DIR = '/tmp/svhn_classifier_logs'
 NUM_LABELS = 10
-SEED = 66478  # Set to None for random seed.
+#SEED = 66478  # Set to None for random seed.
 BATCH_SIZE = 1024
-#NUM_EPOCHS = 256
-NUM_EPOCHS = 50
+NUM_EPOCHS = 1000
+IMG_ROWS = 32
+IMG_COLS = 32
+NUM_CHANNELS = 3
 
 def error_rate(predictions, labels):
   correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(labels, 1))
@@ -32,36 +34,45 @@ def prepare_log_dir():
     tf.gfile.DeleteRecursively(TENSORBOARD_SUMMARIES_DIR)
   tf.gfile.MakeDirs(TENSORBOARD_SUMMARIES_DIR)
 
-def fill_feed_dict(train_data, train_labels, x, y_, step):
-  train_size = train_labels.shape[0]
+def fill_feed_dict(data, labels, x, y_, step):
+  size = labels.shape[0]
   # Compute the offset of the current minibatch in the data.
   # Note that we could use better randomization across epochs.
-  offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
-
-  batch_data = train_data[offset:(offset + BATCH_SIZE), ...]
-  batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
-
+  offset = (step * BATCH_SIZE) % (size - BATCH_SIZE)
+  batch_data = data[offset:(offset + BATCH_SIZE), ...]
+  batch_labels = labels[offset:(offset + BATCH_SIZE)]
   return {x: batch_data, y_: batch_labels}
 
-def train(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path):
+
+def train_classification(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path):
   # This is where training samples and labels are fed to the graph.
+  with tf.name_scope('input'):
+    X = tf.placeholder(tf.float32, shape=[BATCH_SIZE, IMG_ROWS, IMG_COLS, NUM_CHANNELS])
+
+  with tf.name_scope('image'):
+    tf.image_summary('input', X, 10)
+
   y_ = tf.placeholder(tf.float32, shape=[BATCH_SIZE, NUM_LABELS])
 
   # Training computation: logits + cross-entropy loss.
-  x, logits, params = classification_head(BATCH_SIZE, True)
+  training = True
+  logits, params = classification_head(X, BATCH_SIZE, training)
+
   prediction = tf.nn.softmax(logits)
 
-  # L2 regularization for the fully connected parameters.
-  l3_weights = params[4]
-  l3_biases = params[5]
-  l4_weights = params[6]
-  l4_biases = params[7]
-  regularizers = (tf.nn.l2_loss(l3_weights) + tf.nn.l2_loss(l3_biases) + tf.nn.l2_loss(l4_weights) + tf.nn.l2_loss(l4_biases))
+  #L2 regularization for the fully connected parameters.
+  #l3_weights = params[4]
+  #l3_biases = params[5]
+  #l4_weights = params[6]
+  #l4_biases = params[7]
+
+  #Loss
+  #regularizers = (tf.nn.l2_loss(l3_weights) + tf.nn.l2_loss(l3_biases) + tf.nn.l2_loss(l4_weights) + tf.nn.l2_loss(l4_biases))
+  #loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_))
+  # Add the regularization term to the loss.
+  #loss += 5e-4 * regularizers
 
   loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_))
-
-  # Add the regularization term to the loss.
-  loss += 5e-4 * regularizers
 
   # Optimizer: set up a variable that's incremented once per batch and
   # controls the learning rate decay.
@@ -120,61 +131,62 @@ def train(train_data, train_labels, valid_data, valid_labels, test_data, test_la
     for step in xrange(int(NUM_EPOCHS * train_size) // BATCH_SIZE):
       # Run the graph and fetch some of the nodes.
       # This dictionary maps the batch data (as a numpy array) to the
-      feed_dict = fill_feed_dict(train_data, train_labels, x, y_, step)
-      _, l, lr, predictions = sess.run([optimizer, loss, learning_rate, prediction], feed_dict = feed_dict)
+      feed_dict = fill_feed_dict(train_data, train_labels, X, y_, step)
+      training = True 
+      _, l, lr, predictions = sess.run([optimizer, loss, learning_rate, logits], feed_dict = feed_dict)
       duration = time.time() - start_time
 
       if step %  1000 == 0:
-        valid_feed_dict = fill_feed_dict(valid_data, valid_labels, x, y_, step)
-        
+        training = False
+        valid_feed_dict = fill_feed_dict(valid_data, valid_labels, X, y_, step)
         valid_summary, _, l, lr, valid_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=valid_feed_dict, options=run_options, run_metadata=run_metadata)
         valid_writer.add_run_metadata(run_metadata, 'step%03d' % step)
         valid_writer.add_summary(valid_summary, step)
-        
+
         train_summary, _, l, lr, predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict = feed_dict)
         train_writer.add_run_metadata(run_metadata, 'step%03d' % step)
         train_writer.add_summary(train_summary, step)
 
         print('Adding run metadata for', step)
-        print('Validation Accuracy: %.2f%%' % error_rate(valid_predictions, feed_dict[y_]))
+        print('Validation Accuracy: %.2f%%' % error_rate(valid_predictions, valid_feed_dict[y_]))
 
-      if step % 10 == 0:
+      if step % 100 == 0:
         elapsed_time = time.time() - start_time
         start_time = time.time()
         examples_per_sec = BATCH_SIZE / duration
         format_str = ('%s: step %d, loss = %.2f  learning rate = %.2f  (%.1f examples/sec; %.3f ''sec/batch)')
         print (format_str % (datetime.now(), step, l, lr, examples_per_sec, duration))
+        train_error_rate = error_rate(predictions, feed_dict[y_])
+        print('Mini-Batch Accuracy: %.2f%%' % train_error_rate)
         sys.stdout.flush()
 
     #Save the variables to disk.
-    save_path = saver.save(sess, "/tmp/model.ckpt")
+    save_path = saver.save(sess, "/tmp/classifier.ckpt")
     print("Model saved in file: %s" % save_path)
-    test_feed_dict = fill_feed_dict(test_data, test_labels, x, y_, step)
+    training = False 
+    test_feed_dict = fill_feed_dict(test_data, test_labels, X, y_, step)
     _, l, lr, test_predictions = sess.run([optimizer, loss, learning_rate, prediction], feed_dict=test_feed_dict, options=run_options, run_metadata=run_metadata)
     test_summary, _, l, lr, test_predictions = sess.run([merged, optimizer, loss, learning_rate, prediction], feed_dict=test_feed_dict, options=run_options, run_metadata=run_metadata)
     test_writer.add_run_metadata(run_metadata, 'step%03d' % step)
     test_writer.add_summary(test_summary, step)
 
+    test_error_rate = error_rate(test_predictions, test_feed_dict[y_])
+    print('Test Accuracy: %.2f%%' % test_error_rate)
+
     train_writer.close()
     valid_writer.close()
     test_writer.close()
-    #test_error = error_rate(eval_in_batches(test_data, sess, eval_prediction, tf_data_node), test_labels)
-    #print('Test Accuracy: %.2f%%' % test_error)
+
 
 def main(saved_weights_path):
   prepare_log_dir()
   train_data, train_labels= load_svhn_data("train", "cropped")
   valid_data, valid_labels = load_svhn_data("valid", "cropped")
   test_data, test_labels = load_svhn_data("test", "cropped")
-  print("Cropped data loaded.")
-  #load full sized data
-  train_data_full, train_labels_full= load_svhn_data("train", "full")
-  test_data_full, test_labels_full = load_svhn_data("test", "full")
-  #valid_data_full, valid_labels_full = load_svhn_data("validation", "full")
-  print("Full data loaded.")
   train_size = train_labels.shape[0]
-  train(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path)
+  train_classification(train_data, train_labels, valid_data, valid_labels, test_data, test_labels, train_size, saved_weights_path)
   
+
 if __name__ == '__main__':
   saved_weights_path = None
   if len(sys.argv) > 1:
